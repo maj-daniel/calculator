1d41926fc112cffb40d5bb0084d3a598eda41e62
maj-daniel <daniel.soareslb@gmail.com>
2023-02-19 17:57:25 -0300

    Feat: make simple calculator ui
    In progress

--- js/calculator.js
+++ js/calculator.js
@@ -13,3 +13,16 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, operator){
+    switch(operator){
+        case "add":
+            return add(a, b);
+        case "subtract":
+            return subtract(a, b);
+        case "multiply":
+            return multiply(a, b);
+        case "divide":
+            return divide(a, b);
+    }
+}
